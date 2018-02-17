f0754ca1ae9820a901f7272be16398e3112b8ce7
meganmcgeee <meganpmcgee@gmail.com>
2018-02-16 22:02:51 -0800

    Removed UI folder. Experimenting with iFrames/

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,42 @@
 <template>
   <section id="app">
-    <!-- Bot Window -->
+      <!-- Bot Window -->
         <div id="dialog-frame">
-         <!-- Chat window -->
-          <table v-for="a in answers" id="bot-says">
+          <!-- Chat window -->
+          <table v-for="a in answers">
           <!-- User messages -->
             <tr>
-              <td>{{a.result.resolvedQuery}}</td>
+              <td id="user-says">{{a.result.resolvedQuery}}</td>
             </tr>
           <!-- Bot messages -->
-             <tr>
-              <td>
+              <tr>
+              <td id="bot-says">
           <!-- Bot message types / Speech -->
                 <div v-if="a.result.fulfillment.speech">
                   {{a.result.fulfillment.speech}}
-                </div>
-          <!-- Google Assistant output -->
-                <div v-for="r in a.result.fulfillment.messages">
-
-            <!-- Card Answer-->
-                  <div v-if="r.type == 'basic_card'">
-                    <img :title="r.image.accessibilityText" :alt="r.image.accessibilityText" :src="r.image.url" v-if="r.image">
-                    <section>
-                      <h1>{{r.title}}</h1>
-                      <br>
-                        <h2>{{r.subtitle}}</h2>
-                    </section>
-                    <section>
-                      {{r.formattedText}}
-                    </section>
-                    <section v-for="button in r.buttons">
-                      <a target="_blank" :href="button.openUrlAction.url">{{button.title}}>
-                        <i>Open in new window</i>
-                      </a>
-                    </section>
-                  </div>
-            <!-- Link Answer -->
-                  <div v-if="r.type == 'link_out_chip'" class="chips">
-                    <a class="suggestion link" :href="r.url" target="_blank">
-                      {{r.destinationName}} <i class="openlink">open_in_new</i>
-                    </a>
-                  </div>
-            <!-- Suggestion Chip -->
-                  <div v-if="r.type == 'suggestion_chips'" class="chips">
-                    <div class="suggestion" @click="autosubmit(s.title)" v-for="s in r.suggestions">
-                      {{s.title}}
-                    </div>
-                  </div>
                 </div>
               </td>
             </tr>
           </table>
         <!-- User Input Box and Send Button -->
         <div class="query" id="user-input">
-          <input aria-label="Ask me something" autocomplete="off" v-model="query" class="queryform" @keyup.enter="submit()" placeholder="Ask me something..." autofocus type="text">
+          <input aria-label="Ask me something" 
+            autocomplete="off" 
+            v-model="query" 
+            class="queryform" 
+            @keyup.enter="submit()" 
+            placeholder="Ask me something..." 
+            autofocus type="text">
           <button>Send</button>
         </div>
       </div>
-      <!-- <div id="fade" class="black_overlay"></div> -->
+    </div>
+    <!-- <div id="fade" class="black_overlay"></div> -->
   </section>
 </template>
 
-// Style Imports
+// Style 
 <style lang="sass" scoped>
-  #app
-    max-width: 300px
-    font: 1em sans-serif
 </style>
 
 // Bot Logic
@@ -83,15 +54,6 @@
         speech: 'Go ahead, I am listening',
       }
     },
-    watch: {
-      answers: function(val){
-        setTimeout(() => {
-          document.querySelector('.query').scrollIntoView({
-            behavior: 'smooth'
-          })
-        }, 2)
-      }
-    },
     methods: {
       submit(){
         client.textRequest(this.query).then((response) =>{
@@ -105,7 +67,6 @@
     }
 
   }
-
 </script>
 
 
--- src/main.js
+++ src/main.js
@@ -5,3 +5,21 @@ new Vue({
   el: '#app',
   render: h => h(App)
 }).$mount('#app')
+
+// Vue.component('modal', {
+//   template: '#modal-template',
+//   props: {
+//     show: {
+//       type: Boolean,
+//       required: true,
+//       twoWay: true
+//     }
+//   }
+// });
+
+// new Vue({
+//   el: '#modal-app',
+//   data: {
+//     showModal: false
+//   }
+// });
